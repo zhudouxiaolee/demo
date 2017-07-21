364a9b73ea33954f68bca1ecb8bc5186fa1cf2c8
echo <15829387751@163.com>
2017-07-21 16:22:44 +0800

    修改到线上的数据库

--- myoa/config/db.php
+++ myoa/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=myoasystems',
+    'dsn' => 'mysql:host=60.205.189.102;dbname=myoasystems',
     'username' => 'oauser',
     'password' => 'user#for!oa',
     'charset' => 'utf8',
