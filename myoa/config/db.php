e2a9b9c514b513499f05085096facd276d884afd
mir.echo <15829387751@163.com>
2017-07-31 15:53:08 +0800

    增加线上db.php,web.php文件

---
+++ myoa/config/db.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=172.17.72.203;dbname=myoasystems',
+    'username' => 'oauser',
+    'password' => 'user#for!oa',
+    'charset' => 'utf8',
+    'tablePrefix' => 'oa_',
+    'enableSchemaCache' => true,
+    'schemaCacheDuration' => 3600,
+    'schemaCache' => 'cache'
+];
