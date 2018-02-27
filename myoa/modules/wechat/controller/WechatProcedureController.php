5235151af16a046ff1fde5dd97d318a44092c9f2
echo <15829387751@163.com>
2018-02-27 15:42:09 +0800

    获取openid

--- myoa/modules/wechat/controller/WechatProcedureController.php
+++ myoa/modules/wechat/controller/WechatProcedureController.php
@@ -20,6 +20,8 @@ class WechatProcedureController extends Controller
      * 禁止csrf校验
      */
     public $enableCsrfValidation = false;
+    const APP_ID = 'wxcf4886e8e5ea7fe4';//输入小程序appid
+    const APP_SECRET = 'ab34b420e9db0fd21d48aaf9594a4d0e';//输入小程序app_secret
 
     /**
      * init.
@@ -33,45 +35,30 @@ class WechatProcedureController extends Controller
      */
     public function init(){}
 
-    public function actionGetOpenid() {
+    public function actionGetSessionKey() {
         $com = Yii::$app->request;
         if($com->getIsGet() && $getData = $com->get()) {
             $url = "https://api.weixin.qq.com/sns/jscode2session";
-            $result = $this->curl($url, $getData);
-            $appid = $getData['appid'];
-            $sessionKey = $result['session_key'];
+            $params = ['appid' => self::APP_ID, 'secret' => self::APP_SECRET] + $getData;
+            $result = $this->curl($url, $params);
+            $this->asJson($result);
+        }
+    }
 
-            $encryptedData="CiyLU1Aw2KjvrjMdj8YKliAjtP4gsMZM
-                QmRzooG2xrDcvSnxIMXFufNstNGTyaGS
-                9uT5geRa0W4oTOb1WT7fJlAC+oNPdbB+
-                3hVbJSRgv+4lGOETKUQz6OYStslQ142d
-                NCuabNPGBzlooOmB231qMM85d2/fV6Ch
-                evvXvQP8Hkue1poOFtnEtpyxVLW1zAo6
-                /1Xx1COxFvrc2d7UL/lmHInNlxuacJXw
-                u0fjpXfz/YqYzBIBzD6WUfTIF9GRHpOn
-                /Hz7saL8xz+W//FRAUid1OksQaQx4CMs
-                8LOddcQhULW4ucetDf96JcR3g0gfRK4P
-                C7E/r7Z6xNrXd2UIeorGj5Ef7b1pJAYB
-                6Y5anaHqZ9J6nKEBvB4DnNLIVWSgARns
-                /8wR2SiRS7MNACwTyrGvt9ts8p12PKFd
-                lqYTopNHR1Vf7XjfhQlVsAJdNiKdYmYV
-                oKlaRv85IfVunYzO0IKXsyl7JCUjCpoG
-                20f0a04COwfneQAGGwd5oa+T8yO5hzuy
-                Db/XcxxmK01EpqOyuxINew==";
-
-            $iv = 'r7BXXKkLb8qrSNn05n0qiA==';
-
+    public function actionGetOpenid() {
+        $com = Yii::$app->request;
+        if($com->getIsGet() && $getData = $com->get()) {
+            $appid = self::APP_ID;
+            $sessionKey = $getData['session_key'];
+            $encryptedData = $getData['encryptedData'];
+            $iv = $getData['iv'];
             $pc = new WxBizDataCrypt($appid, $sessionKey);
             $errCode = $pc->decryptData($encryptedData, $iv, $data);
-
             if ($errCode == 0) {
-                //print($data . "\n");
                 $this->asJson($data);
             } else {
-                //print($errCode . "\n");
                 $this->asJson($errCode);
             }
-
         }
     }
 }
